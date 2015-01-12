61d5eb5e26fdfb09313cf67c1cc21ea4673d8ca7
andkirby <andkirby@gmail.com>
2015-01-12 02:31:20 +0200

    Added plugin composer file.

---
+++ AndKirby/Composer/MultiRepo/MultiRepoPlugin.php
@@ -0,0 +1,14 @@
+<?php
+namespace AndKirby\Composer\MultiRepo;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class MultiRepoPlugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+
+    }
+}
